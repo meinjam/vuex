7330b5d59b79f43f9fa34a7643b968befe65e994
meinjam <injam.bd.jsr@gmail.com>
2020-05-22 19:24:32 +0600

    state, getters, mutations, actions and mapping

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductListOne />
+    <ProductListTwo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductListOne from "@/components/ProductListOne.vue";
+import ProductListTwo from "@/components/ProductListTwo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ProductListOne,
+    ProductListTwo,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for="(product, index) in saleProduct" :key="index">
+        <span class="name">{{ product.name }} &nbsp;</span>
+        <span class="price">£{{ product.price }}</span>
+      </li>
+    </ul>
+    <button @click="reducePrice(4)">Reduce Price</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+// import { mapGatters } from "vuex";
+
+export default {
+  name: "ProductListOne",
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+    saleProduct() {
+      return this.$store.getters.saleProduct;
+    },
+    // ...mapGatters(["saleProduct"]),
+  },
+  methods: {
+    // reducePrice(amount) {
+    //   this.$store.dispatch("reducePrice", amount);
+    // },
+    ...mapActions(["reducePrice"]),
+  },
+};
+</script>
+
+<style scoped>
+#product-list-one {
+  background: #fff8b1;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+#product-list-one ul {
+  padding: 0;
+}
+#product-list-one li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
+}
+.price {
+  font-weight: bold;
+  color: #e8800c;
+}
+</style>
